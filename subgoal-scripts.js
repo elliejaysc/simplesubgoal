dda2137f1ab5fc40bd4b2b5dbb0d76085d21bb44
Lisa <67008217+elliejaysc@users.noreply.github.com>
2021-03-14 12:34:06 -0400

    Adds watcher to maintain sub count
    Adds a 5 minute interval check for subs to check for any that have fallen off.

--- subgoal-scripts.js
+++ subgoal-scripts.js
@@ -4,41 +4,42 @@ const currentSubs = d.querySelector(".sub-goal__current-subs");
 const goalValue = d.querySelector(".sub-goal__goal-number");
 let subs, goal;
 
+function checkSubs(period, data) {
+	switch (period) {
+	  case "session":
+    	return data["subscriber-session"]["count"];
+      case "week":
+    	return data["subscriber-week"]["count"];
+      case "month":
+    	return data["subscriber-month"]["count"];
+      case "allTime":
+    	return data["subscriber-total"].count;
+    }
+}
+
 window.addEventListener('onWidgetLoad', function (obj) {
   
   	const fieldData = obj.detail.fieldData;
-  	//subs = obj.detail.session.data["subscriber-total"].count;
     data = obj.detail.session.data;
   	goal = fieldData.subGoal;
   	position = fieldData.goalBarPosition;
+  	autoReset = fieldData.autoReset;
     period = fieldData.subPeriod;
   	subs = 0;
   
-  	switch (period) {
-	  case "session":
-    	subs = data["subscriber-session"]["count"];
-		console.log(data["subscriber-session"]["count"]);
-	    break;
-      case "week":
-    	subs = data["subscriber-week"]["count"];
-		console.log(data["subscriber-week"]["count"]);
-	    break;
-      case "month":
-    	subs = data["subscriber-month"]["count"];
-        		console.log(data["subscriber-month"]["count"]);
-	    break;        
-      case "allTime":
-    	subs = data["subscriber-total"].count;
-        		console.log(data["subscriber-month"]["count"]);
-	    break;
-    }
+  	subs = checkSubs(period, data);
   
+  	setInterval(function() {
+	    subs = checkSubs(period, data);
+	}, 5 * 60 * 1000); // 5 * 60 * 1000 milsec = 5 mins
+  
+  	
+  	/*if(autoReset === "yes") {
+    	subs = subs % goal;
+    }*/
   
   	currentSubs.innerText = subs;
   	goalValue.innerText = goal;
-  
-  	//console.log("Session Subs: " + sessionSubs);
-  
   	subBar.style.setProperty('--bar-width', (subs/goal * 100) + '%');
   	
   	if(position === "top")
@@ -55,13 +56,15 @@ window.addEventListener('onEventReceived', function (obj) {
     const listener = obj.detail.listener.split("-")[0];
     const event = obj.detail.event;
   
-  	console.log(obj);
-  
 	if (listener === 'subscriber') {
         subs = parseInt(subs)+1;
+      
+      	/*if(autoReset === "yes") {
+    		subs = subs % goal;
+    	}*/
+      	
       	currentSubs.innerText = subs;
       
       	subBar.style.setProperty('--bar-width', (subs/goal * 100) + '%');
     }
 });
-
